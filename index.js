0d19cc89cc9ba6a0b557ee11cb041109175d51a8
Tyler Gregg <hkrazy888@gmail.com>
2023-07-24 23:33:36 -0600

    list todos

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const Ejs = require('ejs');
 const Hapi = require('@hapi/hapi');
 const Vision = require('@hapi/vision');
+
 const server = Hapi.Server({ port: 3000 });
+
+const todos = []
+
 const rootHandler = (request, h) => {
   return h.view('index', {
-    title: request.server.version,
-    message: 'Hello Ejs!'
+    todos
   });
 };
+
+const submitTodoHandler = (request, h) => {
+  const newTodo = request.payload.todo
+  todos.unshift(newTodo)
+  return `<div>${newTodo}</div>`
+}
+
 const init = async () => {
   await server.register(Vision);
   server.views({
@@ -16,6 +26,7 @@ const init = async () => {
     path: 'templates'
   });
   server.route({ method: 'GET', path: '/', handler: rootHandler });
+  server.route({ method: 'POST', path: '/todo/submit', handler: submitTodoHandler });
   await server.start();
   console.log('Server running at:', server.info.uri);
 };
